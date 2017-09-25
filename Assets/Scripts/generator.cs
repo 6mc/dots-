960c3eb96938a7be88dfacef888dc88ccbeb04cd
mcanmwoo <mcanmutaf@outlook.com>
2017-09-25 18:10:26 +0300

    box collider

---
+++ Assets/Scripts/generator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generator : MonoBehaviour {
+
+
+public GameObject boxes;
+
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("CreateObstacle", 1f, 1.5f);
+		
+
+	}
+	
+
+void CreateObstacle()
+{
+
+//Instantiate(boxes);
+
+Instantiate(boxes, GeneratedPosition(), Quaternion.identity);
+
+}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+Vector2 GeneratedPosition()
+{
+ int x,y,z;
+ x = Random.Range(-6,6);
+ y = Random.Range(7,8);
+
+ return new Vector2(x,y);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+
+   public Vector2 jumpforce = new Vector2 (300,0);
+ public Vector2 axis = new Vector2 (300,0);
+ Rigidbody2D rB2D;
+
+public int i=0;
+	// Use this for initialization
+	void Start () {
+		 rB2D = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if(Input.GetKeyUp("space"))
+		{
+ 
+if(i%2==0){
+
+
+rB2D.velocity=Vector2.zero;
+rB2D.AddForce(jumpforce);
+i++;
+}
+else
+{
+rB2D.velocity=Vector2.zero;
+rB2D.AddForce(axis);
+i++;
+
+
+}
+
+}
+Vector2 screenPosition= Camera.main.WorldToScreenPoint(transform.position);
+
+
+
+if(screenPosition.x >4 || screenPosition.x <-4)
+{
+
+
+
+}
+}
+
+
+
+
+		}
+		
+	
+
